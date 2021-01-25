95bb58374575abef1b7b60f957765855f4740553
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2021-01-25 01:22:48 +0000

    Update User.php
    add raffles() relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function raffles(): BelongsToMany
+    {
+        return $this->belongsToMany(Raffle::class, 'bids')
+            ->using(Bid::class)
+            ->withPivot('pre_count', 'post_count')
+            ->withTimestamps();
+    }
 }
