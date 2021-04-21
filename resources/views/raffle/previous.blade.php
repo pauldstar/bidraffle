469ae3b460e5efd3c4b0e1de4b1a317cc1c71092
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2021-04-21 05:56:09 +0100

    Update previous.blade.php

--- resources/views/raffle/previous.blade.php
+++ resources/views/raffle/previous.blade.php
@@ -9,7 +9,9 @@
             </h2>
             <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
                  data-bs-parent="#accordionFlushExample">
-                @if(isset($wonRaffles) && $wonRaffles->isNotEmpty())
+                @empty($wonRaffles)
+                    <div class="accordion-body text-center">No Raffles Won</div>
+                @else
                     <div class="accordion-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
                         terry
                         richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
@@ -21,9 +23,7 @@
                         of
                         them accusamus labore sustainable VHS.
                     </div>
-                @else
-                    <div class="accordion-body text-center">No Raffles Won</div>
-                @endif
+                @endempty
             </div>
         </div>
         <div class="accordion-item">
@@ -35,7 +35,7 @@
             </h2>
             <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
                  data-bs-parent="#accordionFlushExample">
-                @if(isset($lostRaffles) && $lostRaffles->isNotEmpty())
+                @empty($lostRaffles)
                     <div class="accordion-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
                         terry
                         richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
@@ -49,7 +49,7 @@
                     </div>
                 @else
                     <div class="accordion-body text-center">No Raffles Lost</div>
-                @endif
+                @endempty
             </div>
         </div>
     </div>
