90447f3a5e928d161584bca6b9e045aca0bd24d7
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2021-04-27 13:11:41 +0100

    Create raffle.php

---
+++ resources/lang/en/raffle.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'login' => 'Login to place bids',
+    'closed' => 'Raffle is closed!',
+    'too_late' => 'Too late to start bidding. Raffle is in end-zone.',
+    'bid_expired' => 'Game over! You have no bids left',
+];
