3c3f5779dff790eff528af5514b81ffafcf9b18b
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2021-04-27 13:03:21 +0100

    Create BaseComponent.php
    with toast functions

---
+++ app/Http/Livewire/BaseComponent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Livewire\Component;
+
+class BaseComponent extends Component
+{
+    use AuthorizesRequests;
+
+    public function successToast(string $message): void
+    {
+        $this->toast('success', $message);
+    }
+
+    public function infoToast(string $message): void
+    {
+        $this->toast('info', $message);
+    }
+
+    public function warningToast(string $message): void
+    {
+        $this->toast('warning', $message);
+    }
+
+    public function errorToast(string $message): void
+    {
+        $this->toast('error', $message);
+    }
+
+    private function toast(string $mode, string $message): void
+    {
+        $this->emit('trigger-toast', ['mode' => $mode, 'message' => $message]);
+    }
+}
