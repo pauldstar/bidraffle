d0d17d0b128c7f92cdcc5ea5b5fb58c8474f2d7e
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2021-04-28 08:40:53 +0100

    Update PlaceBidTest.php
    add tests:
    - bid successful pre-end-zone
    - can't bid after bid expires
    - can't start bidding end zone raffle
    - can't bid closed raffle

--- tests/Unit/Http/Livewire/Raffle/PlaceBidTest.php
+++ tests/Unit/Http/Livewire/Raffle/PlaceBidTest.php
@@ -2,23 +2,89 @@
 
 namespace Tests\Unit\Http\Livewire\Raffle;
 
-use App\Http\Livewire\Raffle;
+use App\Http\Livewire\Raffle as RaffleComponent;
+use App\Models\Bid;
+use App\Models\Raffle;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Livewire\Livewire;
-use Livewire\Testing\TestableLivewire;
 use Tests\TestCase;
 
 class PlaceBidTest extends TestCase
 {
-    private TestableLivewire $raffle;
+    use DatabaseTransactions;
 
-    protected function setUp(): void
+    public function testBidSuccessfulPreEndZone()
     {
-        parent::setUp();
-        $this->raffle = Livewire::test(Raffle::class);
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $raffle = Raffle::factory()->create();
+        $user->raffles()->sync($raffle->id);
+
+        $bid = Bid::firstWhere(['user_id' => $user->id, 'raffle_id' => $raffle->id]);
+        $this->assertEmpty($bid->pre_count);
+
+        $this->bid($raffle);
+        $bid->refresh();
+        $this->assertEquals(1, $bid->pre_count);
+
+        $this->bid($raffle);
+        $bid->refresh();
+        $this->assertEquals(2, $bid->pre_count);
+
+        $this->bid($raffle);
+        $bid->refresh();
+        $this->assertEquals(3, $bid->pre_count);
+    }
+
+    private function bid(Raffle $raffle)
+    {
+        Livewire::test(RaffleComponent::class, ['uuid' => $raffle->uuid])
+            ->call('bid')
+            ->assertEmitted('trigger-toast', 'success', __('raffle.bid_successful'));
+    }
+
+    public function testCantBidAfterBidExpires()
+    {
+        $raffle = Raffle::factory()->endZone()->create();
+        $user = User::factory()->create();
+
+        $user->raffles()->sync([
+            $raffle->id => ['pre_count' => 5, 'post_count' => 5]
+        ]);
+
+        $this->actingAs($user);
+
+        Livewire::test(RaffleComponent::class, ['uuid' => $raffle->uuid])
+            ->call('bid')
+            ->assertEmitted('trigger-toast', 'error', __('raffle.bid_expired'));
+    }
+
+    public function testCantStartBiddingEndZoneRaffle()
+    {
+        $raffle = Raffle::factory()->endZone()->create();
+        $this->actingAs(User::factory()->create());
+
+        Livewire::test(RaffleComponent::class, ['uuid' => $raffle->uuid])
+            ->call('bid')
+            ->assertEmitted('trigger-toast', 'error', __('raffle.too_late'));
+    }
+
+    public function testCantBidClosedRaffle()
+    {
+        $raffle = Raffle::factory()->expired()->create();
+        Livewire::actingAs(User::factory()->create());
+
+        Livewire::test(RaffleComponent::class, ['uuid' => $raffle->uuid])
+            ->call('bid')
+            ->assertEmitted('trigger-toast', 'error', __('raffle.closed'));
     }
 
     public function testGuestCantBid()
     {
-        $this->raffle->call('bid')->assertForbidden();
+        Livewire::test(RaffleComponent::class)
+            ->call('bid')
+            ->assertEmitted('trigger-toast', 'error', __('raffle.login'));
     }
 }
