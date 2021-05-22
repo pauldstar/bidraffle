89d4412b319c518e92c4032f234b7ad262bf62e3
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2021-05-22 23:18:56 +0100

    Update timer.js Refactor using alpine.js

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -1,52 +1,61 @@
 'use strict';
 
-/**
- * @param $display - the DOM element that displays time
- * @param scheduledEnd - the timer's initially scheduled end time in seconds
- * @param actualEnd - the timer's actual/extended end time in seconds
- * @constructor
- */
-function Timer($display, scheduledEnd, actualEnd) {
-    this.$display = $display;
-    this.end = scheduledEnd;
-    this.duration = actualEnd - this.now();
+function timer() {
+    return {
+        timer: '--:--:--',
+        duration: 0,
+        scheduledEnd: '',
+        actualEnd: '',
 
-    if (this.expired()) this.duration = scheduledEnd - actualEnd;
-}
+        init() {
+            this.scheduledEnd = this.$refs.timer.dataset.originalClosesAt;
+            this.actualEnd = this.$refs.timer.dataset.closesAt;
+            this.getDuration();
+            this.start();
+        },
 
-Timer.prototype.start = function () {
-    let countdown = setInterval(_ => {
-        this.update();
+        get endZone() {
+            return this.scheduledEnd < this.actualEnd;
+        },
 
-        if (this.expired()) {
-            this.$display.classList.replace('text-success', 'text-danger');
-            clearInterval(countdown);
-        } else this.duration--;
-    }, 1000);
-}
+        get expired() {
+            return this.duration <= 0;
+        },
 
-Timer.prototype.update = function () {
-    this.$display.innerText = this.format();
-}
+        get timerClass() {
+            return {
+                'text-danger': this.expired,
+                'text-warning': this.endZone,
+                'text-success': !(this.expired || this.endZone)
+            };
+        },
 
-Timer.prototype.format = function () {
-    let abs = Math.abs(this.duration),
-        hrs = Math.floor(abs / 3600),
-        mins = Math.floor((abs - (hrs * 3600)) / 60),
-        secs = abs - (hrs * 3600) - (mins * 60),
-        out = this.pad(hrs) + ':' + this.pad(mins) + ':' + this.pad(secs);
+        getDuration() {
+            let now = Math.floor(Date.now() / 1000);
+            this.duration = this.actualEnd - now;
+        },
 
-    return this.expired() ? '-' + out : out;
-}
+        start() {
+            let countdown = setInterval(_ => {
+                this.update();
 
-Timer.prototype.expired = function () {
-    return this.duration <= 0;
-}
+                if (this.expired) clearInterval(countdown);
+                else this.duration--;
+            }, 1000);
+        },
 
-Timer.prototype.now = function () {
-    return Math.floor(Date.now() / 1000);
-}
+        update() {
+            let abs = Math.abs(this.duration),
+                hrs = Math.floor(abs / 3600),
+                mins = Math.floor((abs - (hrs * 3600)) / 60),
+                secs = abs - (hrs * 3600) - (mins * 60),
+                out = this.pad(hrs) + ':' + this.pad(mins) + ':' + this.pad(secs);
 
-Timer.prototype.pad = function (val) {
-    return val < 10 ? '0' + val : val;
+            this.timer = this.expired ? '-' + out : out;
+        },
+
+        pad(val) {
+            return val < 10 ? '0' + val : val;
+        }
+    }
 }
